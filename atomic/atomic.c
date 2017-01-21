b4600843d5021245a75603ee85f4ad411ba015f1
Rahul Rai <raikrahul@gmail.com>
2017-01-21 14:23:24 +0530

    atomic_read reads as an int, across CPUs

--- atomic/atomic.c
+++ atomic/atomic.c
@@ -23,37 +23,56 @@
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE
  */
 
+
+// We do not as of now, have a format specifier  to print atomic_t variables and 
+// their 64 bit variants using the printk , hence expect messages while compiling
+//ref: https://patchwork.kernel.org/patch/9429167/
+
+
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/atomic.h>
 #include <linux/interrupt.h>
 
-#define DEBUG(m) printk("myatomic: " m " current value = %d\n", atomic_read(&atom));
+//
+//#define DEBUG(m) printk("myatomic: " m " current value = %d\n", atomic_read(&atom));
+
+
+#define	MODULE_NAME "Atomic"
+
+
+#define	DEBUG(format, arg...)	\
+do {	\
+	printk(KERN_INFO "%s[%d, %s]:"format"\n", MODULE_NAME, \
+			__LINE__, __FUNCTION__, ##arg);	\
+} while(0)
+
+
 
 static int __init myatomic_init(void)
 {
 	atomic_t atom;
 	printk(KERN_INFO "myatomic: %s\n", __FUNCTION__);
 
-	atomic_set(&atom, 0);	/* initialize */
-	DEBUG("atomic_set");
+	atomic_set(&atom, -100);	/* initialize */
+	DEBUG("atomic_set %d",atomic_read(&atom));
 	atomic_inc(&atom);
-	DEBUG("atomic_inc");
+	DEBUG("atomic_inc %d",atomic_read(&atom));
 	atomic_dec(&atom);
-	DEBUG("atomic_dec");
+	DEBUG("atomic_dec %d", atomic_read(&atom));
 	atomic_add(100, &atom);
-	DEBUG("atomic_add");
-	atomic_sub(50, &atom);
-	DEBUG("atomic_sub");
-	if (atomic_sub_and_test(50, &atom)) {
-		DEBUG("atomic_sub_and_test");
+	DEBUG("atomic_add %d", atomic_read(&atom));
+	atomic_sub(100, &atom);
+	DEBUG("atomic_sub %d", atomic_read(&atom));
+	if (atomic_sub_and_test(-100, &atom)) {
+		DEBUG("atomic_sub_and_test, We substracted -100 from -100  %d", atomic_read(&atom));
 	}
 	if (atomic_inc_and_test(&atom)) {
-		DEBUG("atomic_inc_and_test");
+		DEBUG("atomic_inc_and_test %d", atomic_read(&atom));
 	}
 	if (atomic_dec_and_test(&atom)) {
-		DEBUG("atomic_dec_and_test");
+		DEBUG("atomic_dec_and_test %d", atomic_read(&atom));
 	}
 
 	return 0;
